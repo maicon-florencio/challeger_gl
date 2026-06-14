c3eab3addc37daee2676550b0395b3bd47d09d17
Maicon S Florencio <maiconomg@gmail.com>
2026-06-14 16:43:40 -0300

    commit initial

---
+++ src/main/java/com/stream/globo/GloboApplication.java
@@ -0,0 +1,13 @@
+package com.stream.globo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GloboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stream/globo/GloboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stream.globo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
